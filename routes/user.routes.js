f8bbfba7c5a6514dec413b2e471e0dc6b339a560
Rahul <zentrades@ZenTradess-MacBook-Air.local>
2025-03-10 05:09:16 +0530

    Added auth middleware for user authentication

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { getUser, getUsers } from "../controllers/user.controller.js";
-
+import authorize from "../middlewares/auth.middleware.js";
 const userRouter = Router();
 
 userRouter.get("/", getUsers);
-userRouter.get("/:id", getUser);
+userRouter.get("/:id",authorize, getUser);
 userRouter.post("/", (req, res) => {
   res.send({ title: "Create all users" });
 });
