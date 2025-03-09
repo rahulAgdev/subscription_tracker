979c45032216d8a9c1a319f7a3eb730e0d48d6a9
Rahul <zentrades@ZenTradess-MacBook-Air.local>
2025-03-10 04:39:58 +0530

    Addint auth.controller.js and sign-in sign-up logic.

--- app.js
+++ app.js
@@ -10,9 +10,7 @@ import errorMiddleware from './middlewares/error.middleware.js';
 import cookieParser from 'cookie-parser';
 dotenv.config();
 const app = express();
-app.use(express.json(
-    
-));
+app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser())
 app.use('/api/v1/auth', authRouter)
