1e3a113e65a5577724b6c3dce6b66a5b2f963ea9
Rahul <zentrades@ZenTradess-MacBook-Air.local>
2025-03-10 06:10:27 +0530

    Adding arcjet middleware - Rate Limiter and Bot Limiter

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ import e from 'express';
 import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from './middlewares/error.middleware.js';
 import cookieParser from 'cookie-parser';
+import arcjetMiddleware from './middlewares/arcjet.middleware.js';
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser())
+app.use(arcjetMiddleware)
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/subscriptions', subscriptionRouter)
